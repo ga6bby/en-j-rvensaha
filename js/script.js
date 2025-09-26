41390b0731e0644010de1937b18a940fc10068c5
ga6bby <gabbyw303@gmail.com>
2025-09-26 09:04:50 +0300

    upd js

--- js/script.js
+++ js/script.js
@@ -7,9 +7,15 @@ function updateDateTime() {
     var hours = now.getHours();
     var minutes = now.getMinutes();
     var seconds = now.getSeconds();
+
     if (minutes < 10) minutes = '0' + minutes;
     if (seconds < 10) seconds = '0' + seconds;
+
+    var weekdays = ["Sunnuntai", "Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai", "Lauantai"];
+    var weekday = weekdays[now.getDay()];
+
     var datetimeString = 'Päivämäärä: ' + day + '.' + month + '.' + year + ' | Kellonaika: ' + hours + ':' + minutes + ':' + seconds;
+    
     var datetimeElement = document.getElementById("current-datetime");
     if (datetimeElement) {
         datetimeElement.innerText = datetimeString;
