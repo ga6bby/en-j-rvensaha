eac91565b71ac979544628781fadafabe5c0acb6
ga6bby <gabbyw303@gmail.com>
2025-09-26 09:11:26 +0300

    js

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ function updateDateTime() {
     var weekdays = ["Sunnuntai", "Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai", "Lauantai"];
     var weekday = weekdays[now.getDay()];
 
-    var datetimeString = weekday + day + '.' + month + '.' + year + ' | Kellonaika: ' + hours + ':' + minutes + ':' + seconds;
+    var datetimeString = weekday '.' + day + '.' + month + '.' + year + ' | Kellonaika: ' + hours + ':' + minutes + ':' + seconds;
 
     var datetimeElement = document.getElementById("current-datetime");
     if (datetimeElement) {
